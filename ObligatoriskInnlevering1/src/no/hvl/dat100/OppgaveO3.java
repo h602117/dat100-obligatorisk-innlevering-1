4608a2eefbff4a85eaa87d7b970b2c28926bd523
Anders Fimreite <602117@stud.hvl.no>
2021-09-08 17:25:36 +0200

    O3: fix error on numbers less than one

--- ObligatoriskInnlevering1/src/no/hvl/dat100/OppgaveO3.java
+++ ObligatoriskInnlevering1/src/no/hvl/dat100/OppgaveO3.java
@@ -4,11 +4,9 @@ import static javax.swing.JOptionPane.*;
 
 public class OppgaveO3 {
 	
-	// Works up to 20!
 	private static long factorial(long n) {
-		if (n == 1) {
-			return 1;
-		}
+		if (n == 1) return 1;
+		if (n <= 0) return 0;
 		
 		return n * factorial(n - 1);
 	}
